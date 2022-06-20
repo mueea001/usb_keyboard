9c718d3e5b9725c33d2358528f48fea06bdf8729
Abdul Mueed <mueea001@gmail.com>
2022-06-20 11:15:49 +0100

    added mouse functionality but only works on Linux

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// import 'package:adaptive_theme/adaptive_theme.dart';
+import 'dart:ffi';
+
+import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:flutter/material.dart';
 import 'package:root/root.dart';
 import 'hid_table.dart';
@@ -14,11 +18,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'USB Keyboard',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
       ),
-      home: const MyHomePage(title: 'USB Keyboard'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        accentColor: Colors.amber,
+      ),
+      themeMode: ThemeMode.dark,
+      home: MyHomePage(title: 'USB Keyboard'),
     );
+    // AdaptiveTheme(
+    //   light: ThemeData(
+    //     brightness: Brightness.light,
+    //     primarySwatch: Colors.red,
+    //     accentColor: Colors.amber,
+    //   ),
+    //   dark: ThemeData(
+    //     brightness: Brightness.dark,
+    //     primarySwatch: Colors.blue,
+    //     accentColor: Colors.amber,
+    //   ),
+    //   initial: AdaptiveThemeMode.dark,
+    //   builder: (theme, darkTheme) => MaterialApp(
+    //     title: 'USB Keyboard',
+    //     // theme: ThemeData(
+    //     //   primarySwatch: Colors.blue,
+    //     // ),
+    //     theme: theme,
+    //     darkTheme: darkTheme,
+    //     themeMode: ThemeMode.dark,
+    //     home: MyHomePage(title: 'USB Keyboard'),
+    //   ),
+    // );
   }
 }
 
@@ -51,34 +89,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String createHIDPacket(int modCode, int code1, int code2, int code3,
       int code4, int code5, int code6, int code7) {
-    return "\\\\x" +
-        modCode.toRadixString(16) +
-        "\\\\x" +
-        code1.toRadixString(16) +
-        "\\\\x" +
-        code2.toRadixString(16) +
-        "\\\\x" +
-        code3.toRadixString(16) +
-        "\\\\x" +
-        code4.toRadixString(16) +
-        "\\\\x" +
-        code5.toRadixString(16) +
-        "\\\\x" +
-        code6.toRadixString(16) +
-        "\\\\x" +
-        code7.toRadixString(16);
+    return "\\\\x${modCode.toRadixString(16)}\\\\x${code1.toRadixString(16)}\\\\x${code2.toRadixString(16)}\\\\x${code3.toRadixString(16)}\\\\x${code4.toRadixString(16)}\\\\x${code5.toRadixString(16)}\\\\x${code6.toRadixString(16)}\\\\x${code7.toRadixString(16)}";
   }
 
   /// This function will write to /dev/hidg0
   void sendEvent(String packet) {
-    String cmd = "echo -ne " + packet + " > /dev/hidg0";
+    String cmd = "echo -ne $packet > /dev/hidg0";
     print(cmd);
     Root.exec(cmd: cmd);
   }
 
   void sendKey(hidKeyCode, hidModCode) {
     sendEvent(createHIDPacket(hidModCode, 0, hidKeyCode, 0, 0, 0, 0, 0));
-    keyUp();
+    // keyUp();
   }
 
   void keyUp() {
@@ -86,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _keyPress(String key) {
-    print(key + ": " + keyToKeycode[key.toLowerCase()].toString());
+    print("$key: ${keyToKeycode[key.toLowerCase()]}");
     if (key.length == 1) {
       // int hidKeyCode = key.toLowerCase().codeUnitAt(0) - 93;
       // print(key.toLowerCase().codeUnitAt(0));
@@ -128,6 +151,34 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+                  Joystick(listener: listener),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: GestureDetector(
+                          onTapUp: _clickUp,
+                          onTapDown: (details) => _clickDown(details, "LEFT"),
+                          child: const ElevatedButton(
+                            onPressed: null,
+                            child: Icon(Icons.chevron_left),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: GestureDetector(
+                          onTapUp: _clickUp,
+                          onTapDown: (details) => _clickDown(details, "RIGHT"),
+                          child: const ElevatedButton(
+                            onPressed: null,
+                            child: Icon(Icons.chevron_right),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                   Row(children: [
                     Flexible(
                       flex: 6,
@@ -139,10 +190,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       flex: 1,
                       child: ElevatedButton(
                         onPressed: () => _parseText(textCont.text),
-                        child: const Icon(Icons.send),
                         style: ElevatedButton.styleFrom(
                           primary: Colors.green,
                         ),
+                        child: const Icon(Icons.send),
                       ),
                     )
                   ])
@@ -153,10 +204,22 @@ class _MyHomePageState extends State<MyHomePage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: RegExp(r'\[\w+\]|[\w]')
                             .allMatches(rows)
-                            .map((key) => Flexible(
-                                child: ElevatedButton(
-                                    onPressed: () => _keyPress(key.input
-                                        .substring(key.start, key.end)),
+                            .map(
+                              (key) => Flexible(
+                                child: Listener(
+                                  onPointerDown: (details) {
+                                    _keyPress(key.input
+                                        .substring(key.start, key.end));
+                                  },
+                                  onPointerUp: (details) => keyUp(),
+                                  onPointerCancel: (ev) => keyUp(),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      print(key.input
+                                          .substring(key.start, key.end));
+                                      // _keyPress(key.input
+                                      //     .substring(key.start, key.end));
+                                    },
                                     child:
                                         // Text(
                                         //   key.input.substring(key.start, key.end),
@@ -169,14 +232,18 @@ class _MyHomePageState extends State<MyHomePage> {
                                             k[k.length - 1] == "]") {
                                           return Icon(icons[k]);
                                         }
-                                        return Icon(Icons.cancel);
+                                        return const Icon(Icons.cancel);
                                       } else {
                                         return Text(
                                           key.input
                                               .substring(key.start, key.end),
                                         );
                                       }
-                                    }()))))
+                                    }()),
+                                  ),
+                                ),
+                              ),
+                            )
                             .toList()))
                     .toList() +
                 [
@@ -200,5 +267,66 @@ class _MyHomePageState extends State<MyHomePage> {
                 ]),
       ),
     );
+  }
+
+  Future<void> listener(StickDragDetails details) async {
+    print("${details.x} ${details.y}");
+    int b1 = 0;
+    // if (details.x.isNegative) {
+    //   b1 = b1 | 16;
+    // }
+    // if (details.y.isNegative) {
+    //   b1 = b1 | 32;
+    // }
+    int mult = 16;
+    // Int8 n = 3;
+    int x = (details.x * mult).toInt().toUnsigned(8);
+    int y = (details.y * mult).toInt().toUnsigned(8);
+    var cmd =
+        "echo -ne \\\\x${b1.toRadixString(16)}\\\\x${x.toRadixString(16)}\\\\x${y.toRadixString(16)} > /dev/hidg1";
+
+    await Root.exec(cmd: cmd);
+    print(cmd);
+  }
+
+  Future<void> _clickDown(TapDownDetails details, String button) async {
+    int b1 = 0;
+    if (button == "LEFT") {
+      b1 = 1;
+    } else if (button == "RIGHT") {
+      b1 = 2;
+    }
+    // if (details.x.isNegative) {
+    //   b1 = b1 | 16;
+    // }
+    // if (details.y.isNegative) {
+    //   b1 = b1 | 32;
+    // }
+    // Int8 n = 3;
+    int x = 0;
+    int y = 0;
+    var cmd =
+        "echo -ne \\\\x${b1.toRadixString(16)}\\\\x${x.toRadixString(16)}\\\\x${y.toRadixString(16)} > /dev/hidg1";
+
+    await Root.exec(cmd: cmd);
+    print(cmd);
+  }
+
+  Future<void> _clickUp(TapUpDetails details) async {
+    int b1 = 0;
+    // if (details.x.isNegative) {
+    //   b1 = b1 | 16;
+    // }
+    // if (details.y.isNegative) {
+    //   b1 = b1 | 32;
+    // }
+    // Int8 n = 3;
+    int x = 0;
+    int y = 0;
+    var cmd =
+        "echo -ne \\\\x${b1.toRadixString(16)}\\\\x${x.toRadixString(16)}\\\\x${y.toRadixString(16)} > /dev/hidg1";
+
+    await Root.exec(cmd: cmd);
+    print(cmd);
   }
 }
